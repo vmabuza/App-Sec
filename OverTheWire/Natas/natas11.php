e11cecec8bc1231f950ac47efbfb155eec13e79c
root <root@localhost.localdomain>
2020-02-27 09:07:20 -0500

    fun and games

---
+++ OverTheWire/Natas/natas11.php
@@ -0,0 +1,18 @@
+#!/usr/bin/php
+<?  
+
+$original_cookie = base64_decode('ClVLIh4ASCsCBE8lAxMacFMZV2hdVVotEhhUJQNVAmhSEV4sFxFeaAw');  
+ 
+function xor_encrypt($in) {
+    $text = $in;  
+    $key = json_encode(array( "showpassword"=>"no", "bgcolor"=>"#ffffff"));  
+    $outText = '';  
+    // Iterate through each character  
+    for($i=0;$i<strlen($text);$i++) {  
+    $outText .= $text[$i] ^ $key[$i % strlen($key)];  
+}
+return $outText;  
+}  
+print xor_encrypt($original_cookie);
+
+?>  
